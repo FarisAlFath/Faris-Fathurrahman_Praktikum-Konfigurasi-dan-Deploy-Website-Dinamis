db6eb920c7d6c2fa710a58e7c020c855f100719a
unknown <FarisAlFath>
2026-02-26 15:15:37 +0700

    test

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,7 +1,1 @@
-<x-blog-layout>
-    <!-- Posts Section -->
-    <section class="w-full lg:w-2/3 flex flex-col">
-        <x-post-list :posts="$posts" />
-    </section>
-
-</x-blog-layout>
+<h1 style="background: red; color: yellow; font-size: 100px;">BERUBAH GAK?!</h1>
